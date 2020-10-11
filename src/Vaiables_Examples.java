0a559774ac2f94ae4aa13c51aa78a64086a0aff4
Shey23 <sheyman.ortiz@ulead.ac.cr>
2020-10-10 22:20:38 -0600

    Shey Tarea

---
+++ src/Vaiables_Examples.java
@@ -0,0 +1,9 @@
+public class Vaiables_Examples {
+    public static void main (String[]args){
+        short ValorMaximo= 32767;
+        long ValorMaximo1= 3223_372_035L;
+        double ValorNoEntero= 2.4368244d;
+
+        System.out.print(ValorMaximo + ValorMaximo1 % ValorNoEntero);
+    }
+}
